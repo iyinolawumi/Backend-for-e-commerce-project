4d6738d2ebbd819eb508850c11ed93efc6fd064d
iyinolawumi <olatreasure90@gmail.com>
2026-01-08 16:29:11 +0100

    Adding temporary placeholder codes  in the userroutes file

--- server.js
+++ server.js
@@ -5,17 +5,17 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const userRoutes = require('./routes/userRoutes');
-const productRoutes = require('./routes/productRoutes');
-const orderRoutes = require('./routes/orderRoutes');
+// const productRoutes = require('./routes/productRoutes');
+// const orderRoutes = require('./routes/orderRoutes');
 
 const app = express();
 
 app.use(bodyParser.json());
 
 // Routes
-app.use('/api/users', userRoutes);
-app.use('/api/products', productRoutes);
-app.use('/api/orders', orderRoutes);
+ app.use('/api/users', userRoutes);
+ //app.use('/api/products', productRoutes);
+ //app.use('/api/orders', orderRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
